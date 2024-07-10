cab586b230af869898bd0962e364a2f513f31e0a
Felix Günther <felix.gnthr@gmail.com>
2024-07-10 12:19:50 +0200

    remove unused 'new_data' argument in patch_recorders

--- R/api.R
+++ R/api.R
@@ -369,7 +369,7 @@ get_historicalrecorders <- function(...) {
 #'
 #' @export
 
-patch_recorders <- function(..., recorder_name, new_data, file_path) {
+patch_recorders <- function(..., recorder_name, file_path) {
   # params = list(...)
   new_data <- list(...)
   ecopi_api("PATCH /recorders/{recorder_name}/", recorder_name = recorder_name, new_data = new_data, file_path = file_path)
